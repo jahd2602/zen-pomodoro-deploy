b5c095fc35326f077969ac09d249408cc768b9dc
Jairo Honorio <jairoh@jairoh-macbookpro.roam.corp.google.com>
2020-04-21 12:22:54 -0300

    Deploy to add task list and offline support

---
+++ precache-manifest.5526c86dbb9c35a3d87dda9a28c48fc7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c444e086b6d5e3c3db0dd2c076e46389",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d4e4fc6aac3a53bea35",
+    "url": "./static/css/main.b7249555.chunk.css"
+  },
+  {
+    "revision": "e529a23c7d82dddd0f22",
+    "url": "./static/js/2.5e6a2387.chunk.js"
+  },
+  {
+    "revision": "08bc178854e12ce0be194fa4845dcd41",
+    "url": "./static/js/2.5e6a2387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4e4fc6aac3a53bea35",
+    "url": "./static/js/main.8d77e24c.chunk.js"
+  },
+  {
+    "revision": "8869a8b8e38b413f34f9",
+    "url": "./static/js/runtime-main.728cdd48.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2a4d3262a1fb2334584c7ba7c6782db6.js"
+  "./precache-manifest.5526c86dbb9c35a3d87dda9a28c48fc7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
